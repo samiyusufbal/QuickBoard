7b4d5a1137ab02a3ff4d9a07f380b1198e598e10
Sami Yusuf BAL <126332599+samiyusufbal@users.noreply.github.com>
2025-08-26 10:42:01 +0300

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,19 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "News",
+    links: [
+      { name: "BBC News", url: "https://bbc.com/news" },
+      { name: "CNN", url: "https://cnn.com" },
+      { name: "Reuters", url: "https://reuters.com" },
+    ],
+  },
+  {
+    title: "Tools",
+    links: [
+      { name: "Gmail", url: "https://gmail.com" },
+      { name: "Google Drive", url: "https://drive.google.com" },
+      { name: "Dropbox", url: "https://dropbox.com" },
+    ],
+  },
+];
